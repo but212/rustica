a67fb2baeba2f146146375c0ab5af852e8063aab
but212 <wjddlf211@gmail.com>
2025-02-04 00:20:44 +0900

    Add doctests for Applicative trait

--- src/category/applicative.rs
+++ src/category/applicative.rs
@@ -4,6 +4,115 @@ use crate::fntype::{ApplyFn, SendSyncFn};
 use crate::category::hkt::ReturnTypeConstraints;
 
 /// A trait for applicative functors, which allow function application within a context.
+///
+/// # Example
+///
+/// ```
+/// use rustica::prelude::*;
+///
+/// #[derive(Debug, Clone, PartialEq, Default)]
+/// struct Maybe<T>(Option<T>);
+///
+/// impl<T> Pure<T> for Maybe<T>
+/// where
+///     T: ReturnTypeConstraints,
+/// {
+///     fn pure(value: T) -> Self {
+///         Maybe(Some(value))
+///     }
+/// }
+/// 
+/// impl<T> HKT for Maybe<T>
+/// where
+///     T: ReturnTypeConstraints,
+/// {
+///     type Output<U> = Maybe<U> where U: ReturnTypeConstraints;
+/// }
+/// 
+/// impl<T> Functor<T> for Maybe<T>
+/// where
+///     T: ReturnTypeConstraints,
+/// {
+///     fn map<U, F>(self, f: F) -> Maybe<U>
+///     where
+///         U: ReturnTypeConstraints,
+///         F: SendSyncFnTrait<T, U>,
+///     {
+///         match self.0 {
+///             Some(x) => Maybe(Some(f.call(x))),
+///             None => Maybe(None),
+///         }
+///     }
+/// }
+///
+/// impl<T> Applicative<T> for Maybe<T>
+/// where
+///     T: ReturnTypeConstraints,
+/// {
+///     fn apply<B, F>(self, f: Self::Output<F>) -> Self::Output<B>
+///     where
+///         B: ReturnTypeConstraints,
+///         F: ApplyFn<T, B>,
+///     {
+///         match (self.0, f.0) {
+///             (Some(x), Some(f)) => Maybe(Some(f.call(x))),
+///             _ => Maybe(None),
+///         }
+///     }
+///
+///     fn lift2<B, C, F>(
+///         self,
+///         b: Self::Output<B>,
+///         f: F,
+///     ) -> Self::Output<C>
+///     where
+///         B: ReturnTypeConstraints,
+///         C: ReturnTypeConstraints,
+///         F: ApplyFn<T, SendSyncFn<B, C>>,
+///     {
+///         match (self.0, b.0) {
+///             (Some(a), Some(b)) => Maybe(Some(f.call(a).call(b))),
+///             _ => Maybe(None),
+///         }
+///     }
+///
+///     fn lift3<B, C, D, F>(
+///         self,
+///         b: Self::Output<B>,
+///         c: Self::Output<C>,
+///         f: F,
+///     ) -> Self::Output<D>
+///     where
+///         B: ReturnTypeConstraints,
+///         C: ReturnTypeConstraints,
+///         D: ReturnTypeConstraints,
+///         F: ApplyFn<T, SendSyncFn<B, SendSyncFn<C, D>>>,
+///     {
+///         match (self.0, b.0, c.0) {
+///             (Some(a), Some(b), Some(c)) => Maybe(Some(f.call(a).call(b).call(c))),
+///             _ => Maybe(None),
+///         }
+///     }
+/// }
+///
+/// let maybe_value = Maybe::pure(2);
+/// let maybe_function = Maybe::pure(SendSyncFn::new(|x| x + 3));
+/// let result = maybe_value.apply(maybe_function);
+/// assert_eq!(result, Maybe::pure(5));
+///
+/// let maybe_value1 = Maybe::pure(2);
+/// let maybe_value2 = Maybe::pure(3);
+/// let maybe_function = SendSyncFn::new(|x| SendSyncFn::new(move |y| x + y));
+/// let result = maybe_value1.lift2(maybe_value2, maybe_function);
+/// assert_eq!(result, Maybe::pure(5));
+///
+/// let maybe_value1 = Maybe::pure(2);
+/// let maybe_value2 = Maybe::pure(3);
+/// let maybe_value3 = Maybe::pure(4);
+/// let maybe_function = SendSyncFn::new(|x| SendSyncFn::new(move |y| SendSyncFn::new(move |z| x + y + z)));
+/// let result = maybe_value1.lift3(maybe_value2, maybe_value3, maybe_function);
+/// assert_eq!(result, Maybe::pure(9));
+/// ```
 pub trait Applicative<A>: Functor<A> + Pure<A>
 where
     A: ReturnTypeConstraints,
@@ -55,7 +164,7 @@ where
     /// - `self`: The applicative functor instance.
     /// - `b`: A wrapped value of type `B`.
     /// - `c`: A wrapped value of type `C`.
-    /// - `f`: A ternary function that takes values of type `A`, `B`, and `C` and returns a value of type `D`.
+    /// - `f`: A ternary function that takes values of type `A`, `B
     ///
     /// # Returns
     /// A new applicative functor containing the result of applying the function `f` to the wrapped values.
